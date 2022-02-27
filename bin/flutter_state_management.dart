79fe9ca03ed835b665586e56e987a9b0c14fd426
isshort <namatullahwahidi@gmail.com>
2022-02-27 13:08:33 +0600

    Stream

--- bin/flutter_state_management.dart
+++ bin/flutter_state_management.dart
@@ -1,3 +1,17 @@
+ 
 void main(List<String> arguments) {
-  print('Hello world!');
+  
+  Stream<int> stream=woodStream();
+  stream.listen((data){
+    print("Your reciver data ${data.toString()}");
+
+  });
+
+}
+
+Stream<int> woodStream() async*{
+  for(int i=0;i<=10;i++){
+    await Future.delayed(Duration(seconds:2));
+    yield i;
+  }
 }
